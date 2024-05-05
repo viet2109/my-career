40c5e0a00264891150f90c59a8334e9282c639dd
Vund <vund1@runsystem.net>
2024-05-06 04:55:00 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -14,9 +14,6 @@ import routes from "./config/routes";
 
 function App() {
   const user = useSelector((state) => state.auth.login.currentUser);
-  useEffect(() => {
-    localStorage.clear();
-  }, []);
   return (
     <Router>
       <Loading />
